dba3e57b6ec5d115ef32e691f15d645a85f406f2
emilykmcullen <emilycullen135@gmail.com>
2021-02-16 17:51:22 +0000

    added listener for authentication success

---
+++ src/main/java/com/example/userManagement/listener/AuthenticationSuccessListener.java
@@ -0,0 +1,28 @@
+package com.example.userManagement.listener;
+
+
+import com.example.userManagement.domain.User;
+import com.example.userManagement.service.LoginAttemptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationSuccessListener {
+
+    private LoginAttemptService loginAttemptService;
+
+    @Autowired
+    public AuthenticationSuccessListener(LoginAttemptService loginAttemptService) {
+        this.loginAttemptService = loginAttemptService;
+    }
+
+    public void onAuthenticationSuccess(AuthenticationSuccessEvent event){
+        Object principal = event.getAuthentication().getPrincipal();
+        if(principal instanceof User){
+            User user = (User) event.getAuthentication().getPrincipal();
+            //turn the object into user
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+        }
+    }
+}
