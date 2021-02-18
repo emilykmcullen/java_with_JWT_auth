3d32b20d7d928c0b9ac163f782c700e30d4c8557
emilykmcullen <emilycullen135@gmail.com>
2021-02-18 10:42:09 +0000

    new methods added to user service

--- src/main/java/com/example/userManagement/service/UserService.java
+++ src/main/java/com/example/userManagement/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.userManagement.domain.User;
 import com.example.userManagement.exception.domain.EmailExistException;
 import com.example.userManagement.exception.domain.UserNotFoundException;
 import com.example.userManagement.exception.domain.UsernameExistException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.util.List;
@@ -17,4 +18,16 @@ public interface UserService {
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role,
+                    boolean isNotLocked, boolean isActive, MultipartFile profileImage);
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail, String role,
+                    boolean isNotLocked, boolean isActive, MultipartFile profileImage);
+
+    void deleteUser(long id);
+
+    User resetPassword(String username, String newPassword);
+
+    User updateProfileImage(String username, MultipartFile profileImage);
 }
